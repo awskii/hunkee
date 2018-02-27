3cc72e0386e43291f836fa0096b63fd26c634f3b
Artem Tsebrovskiy <artem.tsskiy@gmail.com>
2018-02-27 13:36:40 +0300

    added test log file and example update

--- example/main.go
+++ example/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"sync"
 	"time"
+
+	"bufio"
+	"log"
+	"os"
 
 	"github.com/awskii/hunkee"
 )
@@ -13,31 +17,54 @@ type Beach struct {
 	LoAc uint8     `hunk:"lo_ac"`
 	Temp float32   `hunk:"temp"`
 	Time time.Time `hunk:"time"`
-	next int
 }
 
-var (
-	format = ":time :id :name :lo_ac :temp"
-	entry  = "02 Jan 06 15:04 MST 17522 Brighton 20 25.6"
-)
+var format = ":time :id :name :lo_ac :temp "
 
 func main() {
 	var err error
 	bch := new(Beach)
 
-	workersAmount := 10
-	parser, err := hunkee.NewParser(format, bch, workersAmount)
+	parser, err := hunkee.NewParser(format, bch)
 	if err != nil {
 		panic(err)
 	}
 
-	iters := 100000000
-	for i := 0; i < iters; i++ {
-		if i%10000 == 0 {
-			fmt.Printf("\r Progress: %d/%d", i, iters)
-		}
-		if err := parser.ParseLine(entry, bch); err != nil {
-			fmt.Println(err)
-		}
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Panic(err)
 	}
+	r := bufio.NewReader(f)
+
+	data := make([]*Beach, 0, 100)
+	result := make(chan *Beach)
+	wg := new(sync.WaitGroup)
+
+	for {
+		line, err := r.ReadString('\n')
+		if err != nil {
+			// io.EOF expected
+			break
+		}
+
+		wg.Add(1)
+		go func(line string, result chan *Beach) {
+			var b Beach
+			if err := parser.ParseLine(line, &b); err != nil {
+				log.Println(err)
+			}
+			result <- &b
+		}(line, result)
+	}
+
+	go func() {
+		for b := range result {
+			data = append(data, b)
+			wg.Done()
+		}
+	}()
+	wg.Wait()
+	log.Println("Done")
+
+	f.Close()
 }
